4b8edd78081f14738b71d52c7884bd1eaf429704
Benjamin Eder <barny.eder@gmail.com>
2019-01-29 15:02:14 +0100

    Added test data reader

---
+++ best_attempt_eva.py
@@ -0,0 +1,3 @@
+import test_data_reader
+
+test_data_reader.readTestData(file_name="test_data.txt")
---
+++ test_data_reader.py
@@ -0,0 +1,35 @@
+# Read test data from the passed file name.
+def readTestData(file_name):
+    samples = []
+    with open(file_name) as file:
+        for line in file:
+            samples.append(_parse_line(line))
+
+    return samples
+
+
+def _parse_line(line):
+    parts = line.split("], ")
+
+    target_part = parts[0]
+    distances_part = parts[1]
+
+    # Remove all brackets from string
+    target_part = target_part.translate({ord(i): None for i in '[]'})
+    distances_part = distances_part.translate({ord(i): None for i in '[]'})
+
+    target = _parse_number_array(target_part)
+    distances = _parse_number_array(distances_part)
+
+    return [target, distances]
+
+
+def _parse_number_array(string):
+    parts = string.split(",")
+
+    result = []
+    for part in parts:
+        number = float(part)
+        result.append(number)
+
+    return result
