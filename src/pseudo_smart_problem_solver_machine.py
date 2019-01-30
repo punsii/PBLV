c1e148858c3fb5835c6dcfdb3c58e420ab7d3443
Benjamin Eder <barny.eder@gmail.com>
2019-01-30 15:30:32 +0100

    Fixed visualization for custom target

--- src/pseudo_smart_problem_solver_machine.py
+++ src/pseudo_smart_problem_solver_machine.py
@@ -219,7 +219,7 @@ learning_targets, testing_targets = targets[:data_split, :], targets[data_split:
 model = build_model(dimension_count, sensor_count)
 
 # Train model
-model.fit(learning_distances, learning_targets, epochs=10)
+model.fit(learning_distances, learning_targets, epochs=1)
 
 # Test model
 test_loss, test_mae, test_mse = model.evaluate(testing_distances, testing_targets)
